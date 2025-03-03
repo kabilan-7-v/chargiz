b06a8451a6b5bc4e99c68d8cf4c63f5cf66fefbc
kabilan-7-v <kabilanvelmani@gmail.com>
2025-03-03 21:47:46 +0530

    first

--- lib/commonpage.dart
+++ lib/commonpage.dart
@@ -1,4 +1,6 @@
 import 'package:chargiz/Page/HomePage/home_page.dart';
+import 'package:chargiz/Page/Location/location_page.dart';
+import 'package:chargiz/Page/ProfilePage/profile_page.dart';
 import 'package:flutter/material.dart';
 
 class Commonpage extends StatefulWidget {
@@ -10,7 +12,7 @@ class Commonpage extends StatefulWidget {
 
 class _CommonpageState extends State<Commonpage> {
   int index = 0;
-  final List<Widget> pages = [HomePage()];
+  final List<Widget> pages = [HomePage(), LocationPage(), ProfilePage()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +20,40 @@ class _CommonpageState extends State<Commonpage> {
       bottomNavigationBar: Container(
           color: Colors.white,
           height: 60,
-          child: const Row(
-            children: [],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    index = 0;
+                  });
+                },
+                child: Column(
+                  children: [Icon(Icons.home), Text('Home')],
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    index = 1;
+                  });
+                },
+                child: Column(
+                  children: [Icon(Icons.location_on), Text('Home')],
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    index = 2;
+                  });
+                },
+                child: Column(
+                  children: [Icon(Icons.person), Text('Home')],
+                ),
+              )
+            ],
           )),
     );
   }
