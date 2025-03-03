314de1cbf65517ece2e75cb9264e032ff38c0377
kabilan-7-v <kabilanvelmani@gmail.com>
2025-03-04 00:31:46 +0530

    add profile pages

--- lib/Page/ProfilePage/profile_page.dart
+++ lib/Page/ProfilePage/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:chargiz/Page/widget/horizontal_calendar.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -10,8 +11,140 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Profile Page")),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Welcome Kabilan,v",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            HorizontalCalendar(todayHighlightColor: Colors.orangeAccent),
+            Row(
+              children: [
+                SizedBox(
+                  width: 8,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Front Wheel:",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(
+                      "28 psi",
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Rear Wheel:",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(
+                      "30 psi",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Center(
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              "assets/images/bike3.jpg",
+                            ),
+                            fit: BoxFit.cover),
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Column(),
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Rides",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Center(
+              child: Container(
+                height: 100,
+                width: MediaQuery.of(context).size.width - 32,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Center(
+              child: Container(
+                height: 100,
+                width: MediaQuery.of(context).size.width - 32,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Center(
+              child: Container(
+                height: 100,
+                width: MediaQuery.of(context).size.width - 32,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Center(
+              child: Container(
+                height: 100,
+                width: MediaQuery.of(context).size.width - 32,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
